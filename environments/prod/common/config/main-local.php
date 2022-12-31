e019135d286118d5a59773dbc1ece9f102f93af6
Anton Kalochelitis <kalochelitis.a@itcomp.org>
2022-12-31 14:45:39 +0200

    1. add default env; 2. update gitignore; 3. update docker-compose; 4. update environments; 5. add Makefile;

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -2,12 +2,16 @@
 
 return [
     'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+            'cachePath' => '@common/runtime/cache'
+        ],
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
+            'dsn' => 'mysql:host=' . env('MYSQL_DOCKER_HOST') . ';port=' . env('MYSQL_DOCKER_PORT') . ';dbname=' . env('MYSQL_DATABASE'),
+            'username' => env('MYSQL_USER', 'root'),
+            'password' => env('MYSQL_PASSWORD'),
+            'charset' => env('MYSQL_CHARSET'),
         ],
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
