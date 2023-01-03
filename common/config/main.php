a74a295429ffe4724f9d74dfefc4494a54f7718b
Anton Kalochelitis <kalochelitis.a@itcomp.org>
2023-01-03 13:08:27 +0200

    1. add yii2-redis; 2. update projectName;

--- common/config/main.php
+++ common/config/main.php
@@ -2,12 +2,18 @@
 return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => \yii\caching\FileCache::class,
+        ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => env('APP_PROJECT_NAME') . '-redis',
+            'port' => env('REDIS_DOCKER_PORT'),
+            'database' => 0,
         ],
     ],
 ];
